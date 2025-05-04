ea3a20b06e04cdf02c09940030c41f062b7a6436
Tom Kent <tom@teeks99.com>
2025-05-03 20:08:43 -0500

    Always pull base

--- build_img.py
+++ build_img.py
@@ -36,17 +36,6 @@ class Image(object):
     def image(self):
         return f"{self.repo}:{self.tag}"
 
-def update_base_images():
-    if not options.no_update_base:
-        # subprocess.check_call("docker pull ubuntu:precise", shell=True)
-        #subprocess.check_call("docker pull ubuntu:trusty", shell=True)
-        subprocess.check_call("docker pull ubuntu:xenial", shell=True)
-        subprocess.check_call("docker pull ubuntu:bionic", shell=True)
-        subprocess.check_call("docker pull ubuntu:focal", shell=True)
-        subprocess.check_call("docker pull ubuntu:jammy", shell=True)
-        subprocess.check_call("docker pull ubuntu:noble", shell=True)
-
-
 def run_my_cmd(cmd):
     try:
         print(cmd)
@@ -61,7 +50,7 @@ def build(version):
     if options.no_force:
         force = ""
 
-    cmd = f"docker build {force} --tag {image.image} gcc-{version}"
+    cmd = f"docker build --pull {force} --tag {image.image} gcc-{version}"
     run_my_cmd(cmd)
     return image
 
@@ -243,8 +232,6 @@ def run():
     if options.version:
         global versions
         versions = options.version
-
-    update_base_images()
 
     all()
 
